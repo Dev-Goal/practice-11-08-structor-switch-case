62b8e16c892b3a777ea4c9df64e79ee3c49abf34
Admin <lmht721@gmail.com>
2024-06-11 16:56:34 +0700

    add bt2

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let brower = prompt("Enter brower name!");
-switch (brower) {
-  case "Edge":
-    alert("You've got the Edge!");
-    break;
+// -----------------Exercise 1------------------------
+// let brower = prompt("Enter brower name!");
+// switch (brower) {
+//   case "Edge":
+//     alert("You've got the Edge!");
+//     break;
 
-  case "Chrome":
-  case "Firefox":
-  case "Safari":
-  case "Opera":
-    alert("Okay we support these browsers too");
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     alert("Okay we support these browsers too");
+//     break;
+//   default:
+//     alert("We hope that this page looks ok!");
+// }
+
+// -----------------Exercise 2------------------------
+let a = prompt("a?");
+switch (a) {
+  case "0":
+    alert("0");
     break;
-  default:
-    alert("We hope that this page looks ok!");
+  case "1":
+    alert("1");
+    break;
+  case "2":
+  case "3":
+    alert("2,3");
+    break;
 }
